435db93c75f526f1e81cedeb186ebde24f769a3e
Robzlegz <robzlegz@gmail.com>
2020-12-12 12:39:58 +0200

    https://github.com/RobzLegz/Minesweeper

--- app.js
+++ app.js
@@ -1,19 +1,47 @@
 var grid = document.getElementById("grid");
+var checkCell = document.querySelector("clicked");
+const stageButton = document.getElementById('search-btn')
 var message;
+var bombCount = 12;
+var bombsFound;
+var flagcount;
+
+function checkLevelDifficulty(){
+    if (document.getElementById('easy').checked){
+        bombsFound = 11;  
+        flagcount = 12;                  
+    }else{                       
+        bombsFound = 22;
+        flagcount = 23;
+    }
+
+}
 
 generateGrid();
 
 function generateGrid() {
+    checkLevelDifficulty();
+    
+
     document.getElementById("grid").style.pointerEvents = "all";
     grid.innerHTML="";
-    //rows
     for (var i = 0; i < 10; i++) {
         row = grid.insertRow(i);
         for (var j = 0; j < 10; j++) {
             cell = row.insertCell(j);
-            cell.onclick = function() { 
-                clickedCell(this); 
+            cell.onclick = function() {                 
+                if(stageButton.classList.contains('flagging')){ 
+
+                    flaggingCell(this);                  
+
+                }else{
+
+                    clickedCell(this);
+
+                }
+                 
             };
+            
             //for every td adds in data-mine with false value, only mines vill have value of true
             var mine = document.createAttribute("data-mine");       
             mine.value = "false";
@@ -25,20 +53,22 @@ function generateGrid() {
 }
 
 function refresh(){
+    checkLevelDifficulty();
     location.reload();
 }
 
 function addMines() {
     //How many bombs in grid (easy difficulty)
-    var bombCount = 12;
+    
     if (document.getElementById('easy').checked){
         bombCount = 12;
         message = "Maybe try the harder difficulty";
     }else{
         //hard difficulty
-        message = "Real MINESWEEPER";
         bombCount = 23;
+        message = "Real MINESWEEPER";
     }
+    
     //randomises the number of mines and adds them to grid
     for (var i = 0; i < bombCount; i++) {
         var row = Math.floor(Math.random() * 10);
@@ -49,19 +79,26 @@ function addMines() {
     }
 }
 
-function checkIfCompleated() {
+
+function checkIfCompleated(bombsFound) {
     var levelComplete = true;
-    for (var i = 0; i < 10; i++) {
-        for (var j = 0; j < 10; j++) {
-            //if there are cells that has data-mine = false, then level isn't compleated
-            if ((grid.rows[i].cells[j].getAttribute("data-mine") == "false") && (grid.rows[i].cells[j].innerHTML == "")){
+    for (var i=0; i<10; i++) {
+        for(var j=0; j<10; j++) {
+            if ((grid.rows[i].cells[j].getAttribute("data-mine")=="false") && (grid.rows[i].cells[j].innerHTML=="")){ 
                 levelComplete = false;
             }
         }
     }
+    if (bombsFound == 0){
+        alert("You won!" + "..." + message);
+        revealMines();
+        document.getElementById("grid").style.pointerEvents = "none";
+        levelComplete = true;
+    }
     if (levelComplete == true) {
         alert("You won!" + "..." + message);
         revealMines();
+        document.getElementById("grid").style.pointerEvents = "none";
     }
 }
 
@@ -71,20 +108,61 @@ function revealMines() {
             //finds all mines
             var cell = grid.rows[i].cells[j];
             //if data-mine = true, change cells classname to mine
-            if (cell.getAttribute("data-mine")=="true"){
+            if (cell.getAttribute("data-mine") == "true"){
                 cell.className = "mine";
-            }           
+            }
+                      
         }
     }
 }
 
+function searchFlag(){
+    stageButton.classList.toggle('flagging');
+    if(stageButton.classList.contains('flagging')){
+        stageButton.innerText = "Flaging";
+    }else{
+        stageButton.innerText = "Searching";
+    }
+}
+//Add flags to clicked cell
+function flaggingCell(cell){    
+    cell.innerHTML = "🚩";
+    cell.classList.toggle("flagon");
+    if (cell.classList.contains("flagon")){
+        cell.innerHTML = "🚩";
+        flagcount -= 1;
+        if (cell.getAttribute("data-mine") == "true"){
+            bombsFound -= 1;
+        }
+    }else{
+        cell.innerHTML = "";
+        flagcount += 1;
+        if (cell.getAttribute("data-mine") == "true"){
+            bombsFound += 1;
+        }
+    }
+    //checks if you have flags
+    if (flagcount == -1 ){
+        alert("You ran out of flags!");
+        cell.innerHTML = "";
+        flagcount = 0;
+    }      
+    checkIfCompleated(); 
+}
+
+
 function clickedCell(cell) {
-    if (cell.getAttribute("data-mine") == "true") {
-        revealMines();
-        message = "Better luck next time"
-        //when lost make grid unclickable
-        document.getElementById("grid").style.pointerEvents = "none";
-        alert("Game Over" + "........." + message);
+    if (cell.getAttribute("data-mine") == "true") { 
+        if (cell.classList.contains("flagon")){
+            return;
+        }else{
+            revealMines();
+            message = "Better luck next time"
+            //when lost make grid unclickable
+            document.getElementById("grid").style.pointerEvents = "none";
+            alert("Game Over" + "........." + message);
+        }       
+        
     
     } else {
         //adds class "clicked" to clicked cells
@@ -95,13 +173,14 @@ function clickedCell(cell) {
         //check if there is a mine next to opened cell
         for (var i = Math.max(cellRow - 1, 0); i <= Math.min(cellRow + 1, 9); i++) {
             for(var j = Math.max(cellCol -1, 0); j <= Math.min(cellCol + 1, 9); j++) {
-                //if if there is a mine next to opened cell then add 1 to minecount
+                //if there is a mine next to opened cell then add 1 to minecount
                 if (grid.rows[i].cells[j].getAttribute("data-mine") == "true"){
                     mineCount += 1;
                 } 
             }
         }
-        cell.innerHTML = mineCount;
+        cell.innerHTML = mineCount;       
+        
         //if clicked on a cell with no mines around, then open cells that are next to it
         if (mineCount == 0) { 
             for (var i = Math.max(cellRow - 1, 0); i <= Math.min(cellRow + 1, 9); i++) {
@@ -114,4 +193,5 @@ function clickedCell(cell) {
         }
         checkIfCompleated();  
     }
+    
 }
